131d796c41c075073d0b17650c0ad40235a907a5
onluiz <luiz.alberto1723@gmail.com>
2017-08-29 15:23:52 -0300

    adicionando estrutura do footer

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Header from '../header/Header';
+import Footer from '../footer/Footer';
 import './App.css';
 
 const App = () => (
   <MuiThemeProvider>
     <div className="App">
       <Header></Header>
+      <Footer></Footer>
     </div>
   </MuiThemeProvider>
 );
