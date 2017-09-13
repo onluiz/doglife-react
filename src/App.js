3233ee67f2b0b2c70f53b43e5d7d564d58599f79
onluiz <luiz.alberto1723@gmail.com>
2017-09-13 13:16:20 -0300

    adicionando material-ui para a utilização do componente AppBar. Alterando AppBar do muicss para o appBar do material-ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Header from './components/header/Header'
 import DogCardList from './components/dogCardList/DogCardList'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <DogCardList />
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <Header />
+          <DogCardList />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,22 +1,41 @@
 import React, { Component } from 'react'
-import Appbar from 'muicss/lib/react/appbar'
+import AppBar from 'material-ui/AppBar';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import './Header.css'
 
 class Header extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {value: 0};
+    }
+
+    handleChange = (event, index, value) => this.setState({value});
+
     render() {
         return (
             <div>
-                <Appbar>
-                    <table width="100%">
-                        <tbody>
-                            <tr>
-                                <td className="mui--appbar-height">
-                                    <font>Doglife</font>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </Appbar>
+                <AppBar
+                title="Doglife"
+                iconElementLeft={
+                    <IconMenu
+                        iconButtonElement={<IconButton><MoreVertIcon className="MoreVertIconStyle"/></IconButton>}
+                        anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+                        targetOrigin={{horizontal: 'left', vertical: 'top'}}
+                        >
+                        <MenuItem primaryText="Refresh" />
+                        <MenuItem primaryText="Send feedback" />
+                        <MenuItem primaryText="Settings" />
+                        <MenuItem primaryText="Help" />
+                        <MenuItem primaryText="Sign out" />
+                    </IconMenu>
+                }
+                />
+
+                
             </div>
         )
     }
